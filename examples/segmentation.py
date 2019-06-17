07e9cf116571d199132d0b8b1560067fc574c672
fgrelard <gm.florent@gmail.com>
2019-06-17 17:26:21 +0200

    Segmentation with region growing

--- examples/segmentation.py
+++ examples/segmentation.py
@@ -6,33 +6,44 @@ import numpy as np
 import cv2 as cv
 
 from skimage import segmentation
+from skimage import measure
+from sklearn.decomposition import PCA
 
 from skimage.filters import gaussian
-from skimage.morphology import binary_erosion
+from skimage.morphology import binary_erosion, closing, disk
+from skimage.filters import threshold_otsu, rank
+from sklearn import manifold
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--threshold", help="Lower threshold for region growing")
+args = parser.parse_args()
+
+threshold = args.threshold
+
+radius = 1
+selem = disk(radius)
 
 image = nib.load("/mnt/d/MALDI/imzML/MSI_20190419_01/00/peaksel.nii")
 
 img_data = image.get_data()
+
 img_data = np.pad(img_data, (3,3), 'constant')
-
-
 similar_images = seg.find_similar_images(img_data)
+mean_image = np.uint8(cv.normalize(np.average(similar_images, axis=2), None, 0, 255, cv.NORM_MINMAX))
+otsu = threshold_otsu(mean_image)
+labels = measure.label(mean_image > otsu, background=0)
+regionprop = seg.properties_largest_area_cc(labels)
+largest_cc = seg.region_property_to_cc(labels, regionprop)
+seeds = set(((int(coord[0]), int(coord[1])) for coord in regionprop.coords))
 
-
-print(len(similar_images))
-# for k in range(similar_images.shape[-1]):
-#     print(k)
-
-
-fig, ax = plt.subplots(figsize=(7, 7))
-ax.imshow(gaussian(current_img.T,1), cmap=plt.cm.gray)
-ax.plot(edges[:,0], edges[:,1], '.')
-ax.plot(snake_curve[:, 0], snake_curve[:, 1], '-b', lw=3)
-ax.set_xticks([]), ax.set_yticks([])
-ax.axis([0, current_img.shape[1], current_img.shape[0], 0])
-
+list_end = seg.region_growing(similar_images, seeds, threshold)
+x = [elem[0] for elem in list_end]
+y = [elem[1] for elem in list_end]
+new_image = np.zeros_like(mean_image)
+new_image[x, y] = 1
+new_image = closing(new_image, selem)
+fig, ax = plt.subplots(1,2)
+ax[0].imshow(mean_image.T)
+ax[1].imshow(new_image.T)
 plt.show()
-
-
-
-#imzmlio.to_nifti(img_data[..., y_kmeans==2], "/mnt/d/MALDI/imzML/MSI_20190419_01/00/clustered_k3_c2.nii")
--- src/segmentation.py
+++ src/segmentation.py
@@ -4,7 +4,9 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from skimage.measure import find_contours
+from skimage import measure
 import cv2 as cv
+import SimpleITK as sitk
 
 def max_variance_sort(image_maldi):
     x, y = image_maldi.getspectrum(0)
@@ -31,9 +33,37 @@ def normalize(image_maldi):
     norm_img = norm_img.transpose()
     return norm_img
 
+def properties_largest_area_cc(ccs):
+    regionprops = measure.regionprops(ccs)
+    areas = lambda r: r.area
+    argmax = max(regionprops, key=areas)
+    return argmax
+
+def region_property_to_cc(ccs, regionprop):
+    label = regionprop.label
+    cc = np.where(ccs == label, 0, 255)
+    return cc
+
+def region_growing(images, seedList, lower_threshold):
+    seeds = seedList.copy()
+    for index in np.ndindex(images.shape[2:]):
+        current_index = (slice(None), slice(None)) + (index)
+        current_image = images[current_index]
+        current_image = np.uint8(cv.normalize(current_image, None, 0, 255, cv.NORM_MINMAX))
+        sitk_image = sitk.GetImageFromArray(current_image)
+        seg_con = sitk.ConnectedThreshold(sitk_image, seedList=list(seeds),
+                                          lower=int(lower_threshold), upper=255, replaceValue=1)
+
+        np_seg_con = sitk.GetArrayFromImage(seg_con)
+        locations_seeds = np.where(np_seg_con == 1)
+        labels = measure.label(np_seg_con, background=0)
+        regionprop = properties_largest_area_cc(labels)
+        largest_cc = region_property_to_cc(labels, regionprop)
+        seeds = seeds.union(set(((int(coord[0]), int(coord[1])) for coord in regionprop.coords)))
+    return list(seeds)
+
 def find_similar_images(image_maldi):
     norm = normalize(image_maldi)
-    average_img =np.average(norm, axis=1)
     pca = PCA(n_components=5)
     X_r = pca.fit(norm).transform(norm)
     kmeans = KMeans(n_clusters=2, random_state=0)
