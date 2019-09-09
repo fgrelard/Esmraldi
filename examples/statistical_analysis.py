13022379d1e28721d02bcc63d6fbb4925b9ad5b0
fgrelard <gm.florent@gmail.com>
2019-09-09 10:08:01 +0200

    Ratio computation: various fixes+separate src file

--- examples/statistical_analysis.py
+++ examples/statistical_analysis.py
@@ -35,7 +35,7 @@ parser.add_argument("-i", "--input", help="Input MALDI image")
 parser.add_argument("-m", "--mri", help="Input MRI image")
 parser.add_argument("-o", "--output", help="Output image")
 parser.add_argument("-r", "--ratio", help="Compute ratio images", action="store_true")
-parser.add_argument("-t", "--top", help="#Top", default=1)
+parser.add_argument("-t", "--top", help="#Top", default=0)
 args = parser.parse_args()
 
 inputname = args.input
@@ -43,6 +43,9 @@ mriname = args.mri
 outname = args.output
 is_ratio = args.ratio
 top = int(args.top)
+
+if top <= 0:
+    top = None
 
 if inputname.lower().endswith(".imzml"):
     imzml = imzmlio.open_imzml(inputname)
@@ -53,36 +56,50 @@ else:
     mzs = [i for i in range(image.shape[2])]
     mzs = np.asarray(mzs)
 
+mzs = np.around(mzs, decimals=2)
+mzs = mzs.astype(str)
+
 image_mri = sitk.GetArrayFromImage(sitk.ReadImage(mriname, sitk.sitkUInt8)).T
 
 image = imzmlio.normalize(image)
 if is_ratio:
-    ratio_images = fusion.extract_ratio_images(image)
-    image = np.concatenate(image, ratio_images)
-    z = image.shape[-1]
-    mzs += [str(i) + "/" + str(j) for i in range(z-1, 0, -1) for j in range(i)]
-    print(mzs[-1])
+    ratio_images, ratio_mzs = fusion.extract_ratio_images(image, mzs)
+    image = np.concatenate((image, ratio_images), axis=2)
+    mzs = np.concatenate((mzs, ratio_mzs))
+
+np.save("data/ratio_650DJ_06_image.npy", image)
+np.save("data/ratio_650DJ_06_mzs.npy", mzs)
+
 image_norm = seg.preprocess_pca(image)
 mri_norm = seg.preprocess_pca(image_mri)
 print(image_norm.shape)
 
+print("Computing PCA")
 fit_pca = fusion.pca(image_norm)
+
 point = fit_pca.transform(mri_norm)
 print("Explained variance ratio =", fit_pca.explained_variance_ratio_)
 
 weights = fit_pca.explained_variance_ratio_ / np.sum(fit_pca.explained_variance_ratio_)
 weights = [1 for i in range(len(weights))]
-
-X_r, af = fusion.clustering(image_norm, fit_pca)
-labels = af.labels_
-centers = af.cluster_centers_
+X_r = fit_pca.transform(image_norm)
+labels = None
 centers = X_r
-similar_images, similar_mzs = fusion.select_images(fit_pca, image, mzs, mri_norm, labels, centers, weights, None)
+
+if top is not None:
+    af = fusion.clustering(image_norm, X_r)
+    labels = af.labels_
+    centers = af.cluster_centers_
+
+similar_images, similar_mzs = fusion.select_images(fit_pca, image, mzs, mri_norm, centers, weights, labels, None)
+print("Selecting images end")
+
+similar_images = similar_images[:1000]
 itk_similar_images = sitk.GetImageFromArray(similar_images)
 sitk.WriteImage(itk_similar_images, outname)
 
 outname_csv = os.path.splitext(outname)[0] + ".csv"
-np.savetxt(outname_csv, similar_mzs, delimiter=",", fmt="%10.5f")
+np.savetxt(outname_csv, similar_mzs, delimiter=",", fmt="%s")
 #np.save("data/labels_maldi.npy", labels)
 #plt.plot(point[0, 0], point[0, 1], "rx")
 
---
+++ src/fusion.py
@@ -0,0 +1,50 @@
+import numpy as np
+import cv2 as cv
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans, AffinityPropagation
+
+
+def clustering(image, X_r):
+    af = AffinityPropagation(preference=-50).fit(X_r)
+    return af
+
+def pca(image):
+    pca = PCA(n_components=5)
+    fit_pca = pca.fit(image)
+    return fit_pca
+
+def weighted_distance(X, weights):
+    return np.sqrt(np.sum(X**2 * weights))
+
+def select_images(fit_pca, images, mzs, mri_norm, centers, weights, labels, top=1):
+    point = fit_pca.transform(mri_norm)
+    distances = np.array([weighted_distance(center-point, weights) for center in centers])
+    indices = [i for i in range(len(distances))]
+    indices.sort(key=lambda x: distances[x])
+    if top is None:
+        similar_images = images[..., indices].T
+        similar_mzs = mzs[indices]
+    else:
+        indices = np.array(indices)
+        condition = np.any(np.array([labels == indices[i] for i in range(top)]), axis=0)
+        similar_images = images[..., condition].T
+        similar_mzs = mzs[condition]
+    return np.uint8(similar_images), similar_mzs
+
+def extract_ratio_images(image, mzs):
+    z = image.shape[-1]
+    c = 0
+    new_mzs = np.zeros(((z**2-z)//2,), dtype='object')
+    ratio_images = np.zeros((image.shape[0], image.shape[1], (z**2-z)//2), dtype=np.uint8)
+    for i in range(z-1, 0, -1):
+        for j in range(i):
+            first_image = image[..., i]
+            second_image = image[..., j]
+            divided = np.zeros_like(first_image, dtype=np.float64)
+            np.divide(first_image, second_image, out=divided, where=second_image!=0)
+            divided = np.uint8(cv.normalize(divided, None, 0, 255, cv.NORM_MINMAX))
+            ratio_images[..., c] = divided
+            current_ratio = mzs[i] + "/" + mzs[j]
+            new_mzs[c] = current_ratio
+            c += 1
+    return ratio_images, new_mzs
