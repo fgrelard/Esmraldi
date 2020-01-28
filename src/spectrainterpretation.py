960fdc6e0e22f431aa23dda9df13a01e2e6afe1e
Florent Grélard <gm.florent@gmail.com>
2020-01-28 16:02:04 +0100

    Annotation of spectra w.r.t a theoretical spectrum

--- src/spectrainterpretation.py
+++ src/spectrainterpretation.py
@@ -38,5 +38,21 @@ def molecule_adducts_regexp(references, expression, adducts):
         theoretical.update(ion_with_adducts)
     return theoretical
 
-def annotation(observed, theoretical):
-    pass
+def closest_peak(reference_mz, theoretical_spectrum, tolerance):
+    keys = list(theoretical_spectrum.keys())
+    values = list(theoretical_spectrum.values())
+    closest = -1
+    closest_name = None
+    for v in values:
+        diff = abs(reference_mz - v)
+        if diff < tolerance or np.isclose(diff, tolerance):
+            closest = v
+            closest_name = keys[values.index(closest)]
+    return closest_name
+
+def annotation(observed, theoretical, tolerance=0.1):
+    annotated = {}
+    for peak in observed:
+        closest_name = closest_peak(peak, theoretical, tolerance)
+        annotated[peak] = closest_name
+    return annotated
--- tests/testspectrainterpretation.py
+++ tests/testspectrainterpretation.py
@@ -5,7 +5,7 @@ import numpy as np
 import numpy.testing as nptest
 import matplotlib.pyplot as plt
 import src.spectrainterpretation as si
-
+import random
 
 class TestSpectraInterpretation(unittest.TestCase):
     def setUp(self):
@@ -13,6 +13,9 @@ class TestSpectraInterpretation(unittest.TestCase):
         self.naming_fn = lambda i: name + str(i)
         self.adducts = {"1Ac": 42.06, "Fe": 172.6}
         self.references = {"Matrix": 521.02, "AX5": 569}
+        random.seed(a=1)
+        self.observed = [random.randrange(569, 2000) for i in range(200)]
+        print(self.observed)
 
     def test_molecules_from_rule(self):
         mol = si.molecules_from_range(120.3, 836.1, 102.6, self.naming_fn)
@@ -39,6 +42,24 @@ class TestSpectraInterpretation(unittest.TestCase):
         self.references = si.update_spectrum(self.references, mol)
         theoretical = si.molecule_adducts_regexp(self.references, r"AX.*", self.adducts)
         print(theoretical)
+
+    def test_closest_peak(self):
+        closest_peak_1 = si.closest_peak(569, self.references, 0.1)
+        closest_peak_2 = si.closest_peak(569.2, self.references, 0.1)
+        print(closest_peak_1, " ", closest_peak_2)
+
+    def test_annotation(self):
+        naming_fn = lambda i: str(i+1) + "Ac"
+        mol = self.adducts["1Ac"]
+        new_adducts = si.molecules_from_rule(3, mol,  naming_fn)
+        self.adducts = si.update_spectrum(self.adducts, new_adducts)
+        mol = si.molecules_from_range(120.3, 836.1, 102.6, self.naming_fn)
+        self.references = si.update_spectrum(self.references, mol)
+        theoretical = si.molecule_adducts_regexp(self.references, r"AX.*", self.adducts)
+        self.references = si.update_spectrum(self.references, theoretical)
+        annotation = si.annotation(self.observed, self.references, 2)
+
+
 
 if __name__ == "__main__":
     unittest.main()
