275e3bb097d5e49698c260f060fe12cbcc75c288
fgrelard <gm.florent@gmail.com>
2020-04-22 15:33:20 +0200

    Comparison synthetic dataset

--- examples/evaluation/quantify_peak_picking.py
+++ examples/evaluation/quantify_peak_picking.py
@@ -11,6 +11,8 @@ import scipy.sparse as sparse
 from scipy.sparse.linalg import spsolve
 from itertools import *
 import re
+import timeit
+import time
 
 def sorted_alphanumeric(data):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
@@ -26,12 +28,27 @@ def intersection_spectra(theoretical, observed, tol):
         t = np.array(t)
         intersection_th = t[(np.abs(o[:, None] - t) < tol).any(0)]
         fraction_intersection = len(intersection_th) * 1.0 / len(t)
-        # print("mising")
-        # for elem in t:
-        #     if elem not in intersection_th:
-        #         print(elem, o[-10:])
         I.append(fraction_intersection)
     return I
+
+def average_diffs_common_peaks(theoretical, observed, tol):
+    keys = np.unique([elem for t in theoretical for elem in t[0]])
+    D = {k:[] for k in keys}
+    # D = {k:[] for k
+    for i in range(observed.shape[0]):
+        o = observed[i]
+        t = theoretical[i][0]
+        o = o[~np.isnan(o)]
+        t = np.array(t)
+        intersection_observed = o[(np.abs(t[:, None] - o).argmin(axis=1))]
+        for j in range(len(t)):
+            k = t[j]
+            v = intersection_observed[j]
+            D[k].append(v)
+    diffs = []
+    for k,v in D.items():
+        diffs.append(np.abs(np.mean(v) - k))
+    return diffs
 
 def baseline_als(y, lam, p, niter=10):
   L = len(y)
@@ -56,7 +73,15 @@ def build_spectra(inputdir):
         i+=1
     return np.array(spectra)
 
-
+timeit.template = """
+def inner(_it, _timer{init}):
+    {setup}
+    _t0 = _timer()
+    for _i in _it:
+        retval = {stmt}
+    _t1 = _timer()
+    return _t1 - _t0, retval
+"""
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help="Input spectra")
@@ -80,18 +105,28 @@ for x,y in spectra:
     spectra_bc.append([x, I])
     index += 1
 
+def foo():
+    time.sleep(1)
+    return 123
+
+
 spectra_bc = np.array(spectra_bc)
 theoretical = build_spectra(theoreticaldir)
-mzs = sp.spectra_peak_mzs_adaptative(spectra_bc, 1.15, 3000)
+time_prominence, mzs = timeit.timeit(lambda: sp.spectra_peak_mzs_adaptative(spectra_bc, 1.15, 3000), number=1)
 
+diffs = average_diffs_common_peaks(theoretical, mzs, 70)
 inters_prominence = intersection_spectra(theoretical, mzs, 70)
 size_prominence = [len(m) for m in mzs]
-print(inters_prominence, size_prominence)
+print(diffs)
+print(np.mean(diffs))
+print(time_prominence, inters_prominence, size_prominence)
 
-
-mzs_cwt = sp.spectra_peak_mzs_cwt(spectra, 1.35, [1, 2, 5, 10, 20, 50, 100, 500])
+time_cwt, mzs_cwt = timeit.timeit(lambda: sp.spectra_peak_mzs_cwt(spectra_bc, 1.45, [1, 2, 5, 10, 20, 50]), number=1)
 inters_cwt = intersection_spectra(theoretical, mzs_cwt, 70)
+diffs_cwt = average_diffs_common_peaks(theoretical, mzs_cwt, 70)
 size_cwt = [len(m) for m in mzs_cwt]
-print(inters_cwt, size_cwt)
+print(diffs_cwt)
+print(np.mean(diffs_cwt))
+print(time_cwt, inters_cwt, size_cwt)
 
 # print(mzs)
