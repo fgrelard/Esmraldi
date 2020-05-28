a6b1a981b498723422e04f52e8566b6e5737c24c
Florent Grelard <gm.florent@gmail.com>
2020-05-28 17:58:00 +0200

    Add sample files

---
+++ data/Mouse_Urinary_Bladder_PXD001283/download.py
@@ -0,0 +1,24 @@
+import shutil
+import urllib.request as request
+from contextlib import closing
+
+def download(ftp, path_distant, path_local):
+    with closing(request.urlopen(ftp + path_distant)) as r:
+        with open(path_local, 'wb') as f:
+            shutil.copyfileobj(r, f)
+
+ftp = "ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2014/11/PXD001283/"
+imzml = "HR2MSI%20mouse%20urinary%20bladder%20S096.imzML"
+ibd = "HR2MSI%20mouse%20urinary%20bladder%20S096.ibd"
+optical = "HR2MSI%20mouse%20urinary%20bladder%20S096%20-%20optical%20image.tif"
+
+print("Downloading imzML")
+download(ftp, imzml, "ms_image.imzml")
+
+print("Downloading ibd")
+download(ftp, ibd, "ms_image.ibd")
+
+print("Downloading optical image")
+download(ftp, optical, "optical_image.tiff")
+
+print("Done")
---
+++ examples/check_install.py
@@ -0,0 +1,15 @@
+import esmraldi.imzmlio
+
+import esmraldi.spectraprocessing
+import esmraldi.spectrainterpretation
+
+import esmraldi.segmentation
+import esmraldi.registration
+
+import esmraldi.theoreticalspectrum
+import esmraldi.speciesrule
+import esmraldi.fusion
+
+
+print("Esmraldi set-up successful!")
+print("All required dependencies installed")
