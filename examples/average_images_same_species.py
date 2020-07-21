Fix average image species

examples/average_images_same_species.py
@@ -62,18 +62,18 @@ modifications = [mol for mol in species if mol.category=="Modification"]
 theoretical_spectrum = TheoreticalSpectrum(ions, adducts, modifications)
 annotation = si.annotation(mzs, theoretical_spectrum.spectrum, 0.5)
 
-list_names = [v for v in annotation.values() if v is not None]
+list_names = [v[0] for v in annotation.values() if len(v) > 0]
 list_names_str = '\n'.join(list_names)
-pattern = re.compile(r".*_(.*)")
-unique_matches = set(pattern.findall(list_names_str))
-unique_matches.add("AX")
+pattern_species = re.compile(r"(.*?)[0-9]+_.*")
+pattern_adducts = re.compile(r".*_(.*)")
+unique_matches = set(pattern_species.findall(list_names_str))
+unique_matches = unique_matches.union(set(pattern_adducts.findall(list_names_str)))
 for add in unique_matches:
-    s = r".*_" + add
-    if add == "AX":
-        s = r"AX[0-9]+\b(?!_)"
+    s = r".*" + add + ".*"
     pattern = re.compile(s)
     matches = pattern.findall(list_names_str)
-    mz_matches = [k for k, v in annotation.items() for m in matches if v == m]
+    mz_matches = [k for k, v in annotation.items() for m in matches if len(v) > 0 and v[0] == m]
+    print(add, mz_matches)
     condition = np.in1d(mzs, np.array(mz_matches))
     image_reduced = np.mean(image[..., condition], axis=2)
     name = outdir + os.path.sep + add + ".png"