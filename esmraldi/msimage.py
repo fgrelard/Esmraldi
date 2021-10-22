Fix error with average image SparseMatrix

esmraldi/msimage.py
@@ -199,7 +199,7 @@ class MSImageImplementation:
             if value.ndim == self.image.ndim:
                 try:
                     value = self.average_image(key)
-                except:
+                except Exception as e:
                     pass
         if is_still_image and not self.is_maybe_densify:
             spectra = self.spectra[key]
@@ -232,6 +232,10 @@ class MSImageImplementation:
 
     def average_image(self, indices):
         average_image = np.mean(np.take(self.image, indices.flatten(), axis=self.spectral_axis), axis=self.spectral_axis)
+        try:
+            average_image = average_image.todense()
+        except:
+            pass
         return average_image
 
 

gui/maincontroller.py
@@ -168,7 +168,7 @@ class MainController:
         self.mouse_y = 0
         self.z = 0
 
-        nb=10
+        nb = 200000
         mzs = (np.arange(nb)+1)
 
         x = np.random.random((10, 10, nb))