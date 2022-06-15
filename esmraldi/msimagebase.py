Fix normalization + minor edits

esmraldi/msimagebase.py
@@ -82,7 +82,7 @@ class MSImageBase:
             norm_factor = norm_img.flatten()
             num = spectra[:, 1, :]
             denom = norm_factor[:, np.newaxis]
-            np.divide(num, denom, out=num, where=denom!=0)
+            np.divide(num, denom, out=num, where=denom>0)
         if len(spectra.shape) >= 3:
             mean_spectra = sp.spectra_mean(spectra)
         else:

examples/find_structured_images.py
@@ -20,6 +20,7 @@ import cv2 as cv
 import argparse
 import os
 import math
+import pandas as pd
 
 from esmraldi.msimage import MSImage
 from esmraldi.sliceviewer import SliceViewer
@@ -49,8 +50,7 @@ def read_image(image_name):
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help="Input (.nii or imzML)")
 parser.add_argument("--on_sample", help="Whether to consider only on-sample points", action="store_true")
-parser.add_argument("-m", "--mask", help="Mask image (any ITK format)")
-parser.add_argument("-r", "--regions", help="Subregions inside mask", nargs="+", type=str)
+parser.add_argument("-r", "--roc", help="ROC file (.xlsx)")
 parser.add_argument("-n", "--normalization", help="Normalization w.r.t. to given m/z", default=0)
 parser.add_argument("-o", "--output", help="Output segmentation (.nii)")
 parser.add_argument("-f", "--factor", help="Factor for the spatially coherent images")
@@ -64,8 +64,7 @@ outname = args.output
 factor = float(args.factor)
 quantiles = args.quantiles
 quantile_upper = int(args.quantile_upper)
-mask_name = args.mask
-region_names = args.regions
+roc_name = args.roc
 normalization = float(args.normalization)
 
 radius = 1
@@ -91,30 +90,34 @@ else:
 
 print(img_data.shape)
 
+img_data = imzmlio.normalize(img_data)
+
+roc_values_df = pd.read_excel(roc_name)
+mzs = roc_values_df.columns[1:]
+end = 4
+roc_auc_scores = np.array(roc_values_df)[:4, 1:].T
+print(roc_auc_scores)
+value = 0.7
+cond = (roc_auc_scores > 1 - value) & (roc_auc_scores < value)
+indices_roc = np.all(cond, axis=-1)
+indices_roc = np.where(indices_roc)[0]
+
+similar_images, indices, off_sample_image, off_sample_cond = seg.find_similar_images_dispersion(img_data, factor, quantiles=quantiles, in_sample=True, return_indices=True)
 
 
-if mask_name and region_names:
-    mask = read_image(mask_name)
-    regions = []
-    for region_name in region_names:
-        region = read_image(region_name)
-        regions.append(region)
+plt.imsave("off_sample.png", off_sample_image.T)
 
-    norm_img = None
-    if normalization > 0:
-        norm_img = imageutils.get_norm_image(img_data, normalization, mzs)
+im_off = img_data[..., off_sample_cond >= 0.95]
+im_incert = img_data[..., (off_sample_cond < 0.95) & (off_sample_cond > 0.5)]
+im_on = img_data[..., off_sample_cond <= 0.5]
 
-    indices, indices_ravel = fusion.roc_indices(mask, mask.shape, norm_img)
+print(im_on.shape, im_off.shape, im_incert.shape)
+fig, ax = plt.subplots(1)
+label = np.vstack((mzs, off_sample_cond, (off_sample_cond>0.5))).T
+tracker = SliceViewer(ax, np.transpose(img_data, (2, 1, 0)), labels=label)
+fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
+plt.show()
 
-    region_bool = fusion.region_to_bool(regions, indices_ravel, mask.shape)
-    roc_auc_scores = fusion.roc_auc_analysis(img_data, indices, region_bool, norm_img)
-    value = 0.7
-    print(roc_auc_scores)
-    cond = (roc_auc_scores > 1 - value) & (roc_auc_scores < value)
-    indices_roc = np.all(cond, axis=-1)
-    indices_roc = np.where(indices_roc)[0]
-
-similar_images, indices = seg.find_similar_images_dispersion(img_data, factor, quantiles=quantiles, in_sample=True, return_indices=True)
 
 indices = np.where(indices)[0]
 indices = np.intersect1d(indices, indices_roc)

examples/registration.py
@@ -103,6 +103,7 @@ def extract_slice_itk(image, index):
 
 def register2D(fixed, moving, numberOfBins, is_best_rotation=False, array_moving=None, flipped=False, sampling_percentage=0.1, learning_rate=1.1, min_step=0.001, relaxation_factor=0.8):
     to_flip = False
+    print("register 2D")
     # Flip axis and choose best fit during registration
     if flipped:
         # Construct a 3D image
@@ -325,14 +326,14 @@ height = fixed.GetHeight()
 numberOfBins = int(math.sqrt(height * width / bins))
 
 
-
 best_resamplers = []
 flips = []
 if dim_moving == 2:
     best_resampler, to_flip = register2D(fixed, moving, numberOfBins, is_best_rotation, array_moving, flipped, sampling_percentage, learning_rate, min_step, relaxation_factor)
+    print(to_flip)
     out = apply_registration(moving, best_resampler, to_flip)
-    p,r =reg.quality_registration(sitk.Cast(fixed, sitk.sitkUInt8), sitk.Cast(out, sitk.sitkUInt8), 40)
-    print(reg.fmeasure(np.mean(p), np.mean(r)))
+    # p,r =reg.quality_registration(sitk.Cast(fixed, sitk.sitkUInt8), sitk.Cast(out, sitk.sitkUInt8), 40)
+    # print(reg.fmeasure(np.mean(p), np.mean(r)))
     best_resamplers.append(best_resampler)
     flips.append(to_flip)
 
@@ -359,9 +360,10 @@ if out != None:
     cimg = sitk.Compose(simg1, simg2, simg1//3.+simg2//1.5)
 
     if dim_moving == 2:
-        fig, ax = plt.subplots(1, 2)
-        ax[0].imshow(sitk.GetArrayFromImage(moving))
-        ax[1].imshow(sitk.GetArrayFromImage(cimg))
+        fig, ax = plt.subplots(1, 3)
+        ax[0].imshow(sitk.GetArrayFromImage(fixed))
+        ax[1].imshow(sitk.GetArrayFromImage(moving))
+        ax[2].imshow(sitk.GetArrayFromImage(cimg))
     elif dim_moving == 3:
         fig, ax = plt.subplots(1, 1)
         tracker = SliceViewer(ax, sitk.GetArrayFromImage(cimg))
@@ -388,8 +390,8 @@ if registername:
     for register_name in register_image_names:
         best_resampler, flip = get_resampler(is_different_resampler, best_resamplers, flips, i)
         is_imzml = register_name.lower().endswith(".imzml")
-        register, mz = read_image_to_register(register_name, is_imzml, to_flip, moving)
-
+        print("flip", flip)
+        register, mz = read_image_to_register(register_name, is_imzml, to_flip=False, moving=moving)
         register = realign_image(register, moving_before_resize)
 
         if is_resize:
@@ -410,7 +412,6 @@ if registername:
         else:
             outImage = registration_itk(register, fixed, best_resamplers, flips)
             outImages.append(outImage)
-
         i += 1
 
     if is_imzml:

gui/ImageViewExtended.py
@@ -37,6 +37,9 @@ from pyqtgraph.dockarea import DockArea, Dock
 from superqt import QLabeledRangeSlider
 from collections import ChainMap
 
+import tracemalloc
+
+
 def PaintingROI(parent, roi, brush, **kwargs):
     class WrapROI(parent):
         def __init__(self, roi, brush=None):
@@ -125,6 +128,9 @@ class ImageViewExtended(pg.ImageView):
         self.current_image = None
         self.levelMin, self.levelMax = None, None
         self.isNewImage = False
+
+        tracemalloc.start()
+        self.number = 0
 
         super().__init__(parent, name, view, imageItem, *args)
 
@@ -592,6 +598,12 @@ class ImageViewExtended(pg.ImageView):
         #Shows image at previous z-index if in range
         if self.imageDisp.ndim > 2 and previousIndex < self.imageDisp.shape[0] and self.hasTimeAxis():
             self.setCurrentIndex(previousIndex)
+        else:
+            self.get_current_image()
+
+        snapshot = tracemalloc.take_snapshot()
+        snapshot.dump("snapshot"+str(self.number)+".pickle")
+        self.number += 1
 
     def roi_scroll_bar(self, ev):
         """
@@ -936,6 +948,8 @@ class ImageViewExtended(pg.ImageView):
                 self.current_image = self.imageDisp[self.actualIndex]
             else:
                 self.current_image = self.imageDisp[0]
+        else:
+            self.current_image = self.imageDisp
 
     def resetROI(self, event):
         self.previousRoiSize = 10
@@ -952,6 +966,7 @@ class ImageViewExtended(pg.ImageView):
     def normalize_ms(self, new_value=False):
         self.get_current_image()
         current_image = self.current_image
+        normed_image = np.zeros_like(current_image)
         if self.imageItem.image is not None and self.hasTimeAxis():
             if self.ui.normTIC.isChecked():
                 tic = self.image.tic
@@ -970,11 +985,12 @@ class ImageViewExtended(pg.ImageView):
                     cut_off = 0
                 if new_value:
                     norm_img = self.image.get_ion_image_mzs(value)
-                    norm_img[norm_img <= cut_off] = 2**32
+                    norm_img[norm_img <= cut_off] = -1
                     self.image.normalization_image = norm_img
 
             if not self.ui.normOff.isChecked():
-                np.divide(current_image, self.image.normalization_image, out=current_image, where=self.image.normalization_image!=0)
+                np.divide(current_image, self.image.normalization_image, out=normed_image, where=self.image.normalization_image>0)
+                current_image = normed_image
             if new_value:
                 self.buildPlot()
 