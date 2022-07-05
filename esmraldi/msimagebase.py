Gui: fix normalization

esmraldi/msimagebase.py
@@ -2,7 +2,7 @@ import numpy as np
 import esmraldi.spectraprocessing as sp
 
 class MSImageBase:
-    def __init__(self, spectra, mzs=None, tolerance=0, spectral_axis=-1, mean_spectra=None, peaks=None, indexing=None):
+    def __init__(self, spectra, mzs=None, tolerance=0, spectral_axis=-1, mean_spectra=None, peaks=None, indexing=None, is_ppm=True):
         all_mzs = spectra[:, 0, ...]
         if len(spectra.shape) == 3:
             all_mzs = all_mzs[np.nonzero(all_mzs)]
@@ -18,6 +18,7 @@ class MSImageBase:
         self._mean_spectra = mean_spectra
         self._peaks = peaks
         self.indexing = None
+        self.is_ppm = is_ppm
         self.is_maybe_densify = True
         self._normalization_image = None
         self._tic = None
@@ -80,14 +81,21 @@ class MSImageBase:
 
         if norm_img is not None:
             norm_factor = norm_img.flatten()
-            num = spectra[:, 1, :]
-            denom = norm_factor[:, np.newaxis]
-            np.divide(num, denom, out=num, where=denom>0)
+            num = spectra[:, 1]
+            if len(spectra.shape) >= 3:
+                denom = norm_factor[:, np.newaxis]
+                np.divide(num, denom, out=num, where=denom>0)
+            else:
+                denom = norm_factor
+                print("MSImageBase: Not re-computing mean spectra for on the fly image")
         if len(spectra.shape) >= 3:
             mean_spectra = sp.spectra_mean(spectra)
         else:
             mean_spectra = sp.spectra_mean_centroided(spectra, self.mzs)
         return mean_spectra
+
+    def tolerance_ppm(self, mz):
+        return mz*self.tolerance/1e6
 
     def max(self, axis=None, out=None, keepdims=False):
         raise NotImplementedError

esmraldi/msimagefly.py
@@ -8,8 +8,8 @@ import esmraldi.utils as utils
 from esmraldi.msimagebase import MSImageBase
 
 class MSImageOnTheFly(MSImageBase):
-    def __init__(self, spectra, coords=None, mzs=None, tolerance=0, spectral_axis=-1, mean_spectra=None, peaks=None, indexing=None):
-        super().__init__(spectra, mzs, tolerance, spectral_axis, mean_spectra, peaks)
+    def __init__(self, spectra, coords=None, mzs=None, tolerance=0, spectral_axis=-1, mean_spectra=None, peaks=None, indexing=None, is_ppm=True):
+        super().__init__(spectra, mzs, tolerance, spectral_axis, mean_spectra, peaks, is_ppm)
         all_mzs = np.hstack(spectra[:, 0]).flatten()
         if indexing is None:
             self.indexing = utils.indices_search_sorted(all_mzs, self.mzs)
@@ -71,7 +71,11 @@ class MSImageOnTheFly(MSImageBase):
         is_array = (isinstance(key, tuple) and any([iterfunc(elem) for elem in key])) or iterfunc(key)
 
         mz_value = self.mzs[key]
-        tolerance_left, tolerance_right = self.tolerance, self.tolerance
+        if self.is_ppm:
+            tolerance_left = self.tolerance_ppm(mz_value)
+            tolerance_right = tolerance_left
+        else:
+            tolerance_left, tolerance_right = self.tolerance, self.tolerance
         if is_array:
             tolerance_left = np.abs(np.median(mz_value) - np.amin(mz_value))
             tolerance_right = np.abs(np.median(mz_value) - np.amax(mz_value))

gui/ImageViewExtended.py
@@ -984,7 +984,12 @@ class ImageViewExtended(pg.ImageView):
                 except:
                     cut_off = 0
                 if new_value:
-                    norm_img = self.image.get_ion_image_mzs(value)
+                    actual_value = self.image.mzs[np.abs(value - self.image.mzs).argmin()]
+                    if self.image.is_ppm:
+                        tol = self.image.tolerance_ppm(actual_value)
+                    else:
+                        tol = self.image.tolerance
+                    norm_img = self.image.get_ion_image_mzs(actual_value, tol, tol)
                     norm_img[norm_img <= cut_off] = -1
                     self.image.normalization_image = norm_img
 

gui/maincontroller.py
@@ -99,13 +99,13 @@ class WorkerOpen(QObject):
             if os.path.isfile(self.npy_indexing_path):
                 indexing = np.load(self.npy_indexing_path, mmap_mode="r")
 
-            img_data = MSImageOnTheFly(spectra, coords=imzml.coordinates, tolerance=0.003, mean_spectra=mean_spectra, indexing=indexing)
+            img_data = MSImageOnTheFly(spectra, coords=imzml.coordinates, tolerance=14, mean_spectra=mean_spectra, indexing=indexing)
 
             img_data = msimage_for_visualization(img_data)
             return img_data
 
         full_spectra = io.get_full_spectra(imzml)
-        img_data = MSImage(full_spectra, image=None, coordinates=imzml.coordinates, tolerance=0.003)
+        img_data = MSImage(full_spectra, image=None, coordinates=imzml.coordinates, tolerance=14)
         img_data = msimage_for_visualization(img_data)
         return img_data
 
@@ -307,7 +307,7 @@ class MainController:
         # self.open_file("/mnt/d/CouplageMSI-Immunofluo/Scan rate 37° line/synthetic.imzML")
 
         # self.open_file("/mnt/d/CouplageMSI-Immunofluo/Scan rate 37° line/random.imzML")
-        self.open_file("/mnt/d/CouplageMSI-Immunofluo/Scan rate 37° line/20210112_107x25_20um_Mouse_Spleen_DAN_Neg_mode_200-2000mz_70K_Laser37_6p5kV_350C_Slens90_Line_centroid_aligned.imzML")
+        self.open_file("/mnt/d/CouplageMSI-Immunofluo/Scan rate 37° line/20210112_107x25_20um_Mouse_Spleen_DAN_Neg_mode_200-2000mz_70K_Laser37_6p5kV_350C_Slens90_Line_centroid.imzML")
         # self.open_file("/mnt/d/CouplageMSI-Immunofluo/20211102 DAN 5um - laser 39/IF/20211102 Rate3#6-BF 10x - Post scan DAN- washed.tif")
         # self.open_file("/mnt/d/CBMN/random.imzML")
 
@@ -506,8 +506,13 @@ class MainController:
                 else:
                     imageview.image.peaks = mzs_annotated
 
+                groups = sp.index_groups_start_end(imageview.image.peaks, imageview.image.tolerance)
+                before_len = len(imageview.image.peaks)
+                imageview.image.peaks = np.array([np.median(g) for g in groups])
+                after_len = len(imageview.image.peaks)
+                added_len = len(mzs_annotated) - (before_len - after_len)
                 self.display_peaks_mean_spectrum(imageview.image.peaks)
-                msg.setText("Added " + str(len(mzs_annotated)) + " peaks from METASPACE")
+                msg.setText("Added " + str(added_len) + " peaks from METASPACE")
             else:
                 msg.setText("Not a valid METASPACE file.")
                 msg.setInformativeText("Please supply a .csv file exported from METASPACE")